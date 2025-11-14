205e58f3f8a4ede6e91988ef2b46261e5226cc45
Jason Pham <jasontp12323@gmail.com>
2025-11-14 12:30:24 -0800

    Initial project setup

---
+++ ClimeCast/ClimeCastApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClimeCastApp.swift
+//  ClimeCast
+//
+//  Created by csuftitan on 11/14/25.
+//
+
+import SwiftUI
+
+@main
+struct ClimeCastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
